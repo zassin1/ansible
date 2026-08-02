8fe35db60054cc9b3b714132cdd44d53b3c96cc4
Jesse Lopez <135650176+zassin1@users.noreply.github.com>
2026-08-01 19:31:40 -0700

    Configure Azure backend for Terraform
    Added Azure backend configuration for Terraform state management.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-ansible-terraform-state"
+    storage_account_name = "tfstatezassin1ansible"
+    container_name       = "tfstate"
+    key                  = "ansible-blog-dev.tfstate"
+    use_oidc             = true
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
